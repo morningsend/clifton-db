b6c1d37ed931528aacce18adae3686ce1cfaa95b
Zaiyang Li <zl4917@my.bristol.ac.uk>
2019-02-19 15:20:01 +0000

    kv server client grpc plumbing

---
+++ cmd/clienttest/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/zl14917/MastersProject/kvclient"
+	"log"
+)
+
+const PORT = 10030
+
+func main() {
+	client := kvclient.NewClient()
+	log.Println("connecting to server on ")
+	err := client.Connect("localhost", PORT)
+
+	if err != nil {
+		log.Fatalln("failed to connect to server:", err)
+	}
+
+	log.Println("PUT value: <hello:world>")
+	_, err = client.Put("hello", []byte("world"))
+
+	if err != nil {
+		log.Println("failed to put", err)
+	}
+
+}
--- kvclient/client.go
+++ kvclient/client.go
@@ -1,16 +1,62 @@
 package kvclient
 
+import (
+	"context"
+	"fmt"
+	"github.com/zl14917/MastersProject/kv-client-api"
+	"google.golang.org/grpc"
+)
+
 type Client struct {
+	grpcConn      *grpc.ClientConn
+	kvStoreClient kv_client_api.KVStoreClient
 }
 
-func NewClient(host string, port int, keyspace string) *Client {
+func (c *Client) Connect(host string, port uint32) (err error) {
+	//ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", host, port), grpc.WithInsecure())
+
+	if err != nil {
+		return err
+	}
+
+	c.grpcConn = conn
+	c.kvStoreClient = kv_client_api.NewKVStoreClient(conn)
+
 	return nil
 }
 
-func (c *Client) put(key string, data []byte) {
-
+func NewClient() *Client {
+	return &Client{}
 }
 
-func (c *Client) get(key string, data []byte) {
+func (c *Client) Put(key string, data []byte) (ok bool, err error) {
+	result, err := c.kvStoreClient.Put(
+		context.Background(),
+		&kv_client_api.PutReq{
+			Key:   key,
+			Value: data,
+		},
+	)
 
+	if err != nil {
+		return false, err
+	}
+
+	return result.Success, nil
+}
+
+func (c *Client) Get(key string) (data []byte, err error) {
+
+	value, err := c.kvStoreClient.Get(
+		context.Background(),
+		&kv_client_api.GetReq{
+			Key: key,
+		})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return value.Value, nil
 }
